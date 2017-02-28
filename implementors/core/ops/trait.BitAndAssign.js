a673424924663c10f8b56ae4636db48f9233eed2
John Gallagher <jgallagher@bignerdranch.com>
2017-02-28 09:55:42 -0500

    Update documentation

--- implementors/core/ops/trait.BitAndAssign.js
+++ implementors/core/ops/trait.BitAndAssign.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["rusqlite"] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/ops/trait.BitAndAssign.html' title='core::ops::BitAndAssign'>BitAndAssign</a> for <a class='struct' href='rusqlite/struct.OpenFlags.html' title='rusqlite::OpenFlags'>OpenFlags</a>",];
+implementors["rusqlite"] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/ops/trait.BitAndAssign.html' title='trait core::ops::BitAndAssign'>BitAndAssign</a> for <a class='struct' href='rusqlite/struct.OpenFlags.html' title='struct rusqlite::OpenFlags'>OpenFlags</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
